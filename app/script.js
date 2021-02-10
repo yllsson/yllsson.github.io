71566b0bfc9c325098e353c579dc56dd3f29ced8
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-02-10 20:54:03 +0000

    add animation through js

---
+++ app/script.js
@@ -0,0 +1,6 @@
+const titleImgContainer = document.getElementById('titleImages');
+
+document.addEventListener('DOMContentLoaded', () => {
+  titleImgContainer.classList.add('loaded');
+  console.log('loaded', titleImgContainer);
+});
