7ed228969afa3ed1dad60d4b42968595c8685deb
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-02-15 21:13:22 +0000

    add contact section + various animation trials

--- app/script.js
+++ app/script.js
@@ -1,6 +1,19 @@
+// TITLE ANIMATION //
 const titleImgContainer = document.getElementById('titleImages');
 
 document.addEventListener('DOMContentLoaded', () => {
   titleImgContainer.classList.add('loaded');
-  console.log('loaded', titleImgContainer);
+});
+
+// CONTACT LINKS ANIMATION //
+
+const contactLinks = document.querySelectorAll('.contact-link');
+
+contactLinks.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    link.classList.add('active');
+  });
+  link.addEventListener('mouseleave', () => {
+    link.classList.remove('active');
+  });
 });
