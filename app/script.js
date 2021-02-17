8242fe5e0d4637f2162eec8c36956f8d1c873e6d
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-02-17 21:49:53 +0000

    various minor changes

--- app/script.js
+++ app/script.js
@@ -4,16 +4,3 @@ const titleImgContainer = document.getElementById('titleImages');
 document.addEventListener('DOMContentLoaded', () => {
   titleImgContainer.classList.add('loaded');
 });
-
-// CONTACT LINKS ANIMATION (not yet in use)//
-
-// const contactLinks = document.querySelectorAll('.contact-link');
-
-// contactLinks.forEach(link => {
-//   link.addEventListener('mouseenter', () => {
-//     link.classList.add('active');
-//   });
-//   link.addEventListener('mouseleave', () => {
-//     link.classList.remove('active');
-//   });
-// });
