1b3cb754bd0d717483ae86b1189b74556cf31339
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-02-21 16:58:49 +0000

    add project section links and class assignment script

--- app/script.js
+++ app/script.js
@@ -4,3 +4,13 @@ const titleImgContainer = document.getElementById('titleImages');
 document.addEventListener('DOMContentLoaded', () => {
   titleImgContainer.classList.add('loaded');
 });
+
+// PROJECT SECTION CLASS ASSIGNMENT //
+
+const projects = document.querySelectorAll('.project');
+
+projects.forEach((project, index) => {
+  if (index % 2 !== 0) {
+    project.classList.add('projectReversed');
+  }
+});
